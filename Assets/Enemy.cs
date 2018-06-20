ce92638d9a3b9e0a4c7cdd3fb03532ad132b6080
CODE\mail <mail@code.dk>
2018-06-20 20:30:56 +0200

    enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject Target;
+    private NavMeshAgent _navAgent;
+
+	// Use this for initialization
+	void Start () {
+        _navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+
+        _navAgent.SetDestination(Target.transform.position);
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Target = other.gameObject;
+        }
+    }
+}
