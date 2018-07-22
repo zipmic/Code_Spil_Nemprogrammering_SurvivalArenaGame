074b963804d2d186f27517c686af2e5cea3a531a
Christian Skriver Kragegaard <mail@code.dk>
2018-07-22 22:06:50 +0200

    grass

---
+++ Assets/Polish/EffectExamples/AutoDestructOnParticleEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestructOnParticleEnd : MonoBehaviour {
+
+    private ParticleSystem _ps;
+
+
+	// Use this for initialization
+	void Awake () {
+        _ps = GetComponent<ParticleSystem>();
+        Destroy(gameObject, _ps.main.duration);
+	}
+	
+	
+}
